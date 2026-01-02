e122fcff457d6b6763efb46efea0217dd29017cd
zafar0171 <modzafar28@gmail.com>
2026-01-02 20:50:35 +0530

    .sh practice file added with syntax error present

---
+++ practice.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+
+#echo test successfull
+
+
+
+# for i in {1..10}; do
+#     echo "welcome $i times"
+# done
+
+
+
+# if [ $myvar -gt 10 ]; then
+#     echo "myvar is greater than 10"
+# else
+#     echo "myvar is less than 10"
+# fi
+
+
+if [ $x -gt 2 ] && [ $y -lt 10]; then
+    echo "if is true"
+
+else
+    echo "else is true"
+
+fi
